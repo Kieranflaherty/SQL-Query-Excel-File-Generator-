c74c4b1dfaf6441f1cef8043ab89acd87a8e8331
Kieran Flaherty <76489588+Kieranflaherty@users.noreply.github.com>
2022-08-10 14:50:44 +0100

    Add files via upload

---
+++ SQL_SNum.py
@@ -0,0 +1,55 @@
+import pyodbc
+import pandas as pd
+import sys
+from datetime import datetime
+import warnings
+import os
+
+today = datetime.now()
+warnings.filterwarnings('ignore')
+
+#Database Credentials
+server = 'EUIRLBFTTM'
+database = 'BectonDickinson'
+username = 'remote'
+password = 'Becton2018'
+
+#Establish Connection to SQL Database
+print("Connecting to OBI Database...")
+connection = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+print("Connection Successful")
+
+#Setup SQL Queries
+serialnum = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0803] AS _8 WHERE _8.S80304_LsrMark_SeqNum = ? ORDER BY L8_DataTimestamp DESC;"
+ewo = "SELECT * FROM [BectonDickinson].[dbo].[ProcessDataOutput0803] AS _8 WHERE _8.S80304_LsrMark_String LIKE '%?%' ORDER BY L8_DataTimestamp DESC;"
+
+#SERIAL NUMBER SEARCH
+def serial_query(connection, serialnum):
+    print("Executing Query...")
+    cursor = connection.cursor()
+    data = pd.read_sql(serialnum, connection, params = [SN])
+    writer = pd.ExcelWriter('Line_Data_SN'+ SN + '_' + today.strftime('%Y_%m_%d')+'.xlsx', engine = 'xlsxwriter')
+    data.to_excel(writer, sheet_name = 'Line 1')
+    writer.save()
+    print("\nQuery Successful, file has been generated.\nFile is stored here", os.getcwd())
+
+#EWO NUMBER SEARCH
+def ewo_query(connection, ewo):
+    print("\nExecuting Query...")
+    cursor = connection.cursor()
+    data = pd.read_sql(serialnum, connection, params = [EWO])
+    writer = pd.ExcelWriter('Line_Data_SN'+ EWO + '_' + today.strftime('%Y_%m_%d')+'.xlsx', engine = 'xlsxwriter')
+    data.to_excel(writer, sheet_name = 'Line 1')
+    writer.save()
+    print("\nQuery Successful, file has been generated.\nFile is stored here", os.getcwd())
+
+#Select Query
+select_query = input("\nPlease select a search option \nType '1' to search by  Device Serial Number \nType '2' to search by Batch Number \n\nSelect Option and Press Enter:")
+if select_query ==("1"):
+    SN = input("Enter Serial Number:")
+    serial_query(connection, serialnum)
+elif select_query ==("2"):
+    EWO = input("Enter EWO Number:")
+    ewo_query(connection, ewo)
+
+input()
